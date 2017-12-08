acfc5166ab105e78d1a758127a96c685da5cf140
h0lzleim <kontakt.pr0news@gmail.com>
2017-12-08 18:21:16 +0100

    First Update
    Better Extraction and Cleaning

--- python_articleextraction/js/renderScript.js
+++ python_articleextraction/js/renderScript.js
@@ -1,5 +1,8 @@
 var regexurl = "@(https?|ftp)://(-\.)?([^\s/?\.#-]+\.?)+(/[^\s]*)?$@iS";
+var loop = true;
+
 $( document ).ready(function() {
+	try{
 	var baseurl = $('img').first().attr('src');
     	$("img").each(function() {
   		var url = $(this).attr("src");
@@ -13,26 +16,72 @@ $( document ).ready(function() {
 			$(this).attr("src", newurl);
 		}
 	});
-	$('p').each(function() {
-    		var $this = $(this);
-    		if($this.html().replace(/\s|&nbsp;/g, '').length == 0)
-        	$this.remove();
-	});
-	$('a').each(function() {
-    		var $this = $(this);
-    		if($this.html().replace(/\s|&nbsp;/g, '').length == 0)
-        	$this.remove();
-	});
-	$("img:not(:first)").each(function() {
-		if($(this).attr("src") == baseurl){
-			$(this).remove();
-		}
-	});
+	}catch(err){}	
+	
 	$("img").each(function() {
  		if($(this).attr("src")){
+			if($(this).attr("src").endsWith("icon.ico")){
+				$(this).remove();
+			}
 		}
 		else{
 			$(this).remove();
 		}
 	});
+	$("img:not(:first)").each(function() {
+		if($(this).attr("src") == baseurl){
+			$(this).remove();
+		}
+		var dim = getImageDimentions($(this))
+		if(dim.width < 300 || dim.height < 250){
+			$(this).remove();
+		}
+	});
+	if($("div.Focus div").first().children().last().is("h2")){$("div.Focus div").children().last().remove()};
+	$("div.Tagesschau p.more.hidden").nextAll("ul.list").remove();
+	while(loop){
+		loop = false;
+		$('p').each(function() {
+    			checkAndDelete($(this))
+		});
+		$('a').each(function() {
+    			checkAndDelete($(this))
+		});
+		$('li').each(function() {
+    			checkAndDelete($(this))
+		});
+		$('ul').each(function() {
+    			checkAndDelete($(this))
+		});
+		$('span').each(function() {
+    			checkAndDelete($(this))
+		});
+		$('strong').each(function() {
+    			checkAndDelete($(this))
+		});
+		if($("div.Tagesschau").length && $("p.infotext").length) {
+			$('p, a').each(function() {
+    				if($(this).text() == "Download der Videodatei"){$(this).remove()}
+				if($(this).text().endsWith("(h264)")){$(this).remove()}
+				if($(this).text().endsWith("der rechten Maustaste klicken und \"Ziel speichern unter ...\" ausw\u00E4hlen.")){$(this).remove()}
+				if($(this).text().endsWith("mp3")){$(this).remove()}
+				if($(this).text().startsWith("Ogg")){$(this).remove()}
+				if($(this).text().endsWith("HTML-Code kopieren und auf ihrer Seite einfügen.\n")){$(this).remove()}
+				if($(this).text().endsWith("Video in folgenden Formaten zum Download an:")){$(this).remove()}
+				if($(this).text().endsWith("Audio in folgenden Formaten zum Download an:")){$(this).remove()}
+			});
+		}
+	}
 });
+function getImageDimentions(imageNode) {
+  var source = imageNode.src;
+  var imgClone = document.createElement("img");
+  imgClone.src = source;
+  return {width: imgClone.width, height: imgClone.height}
+}
+function checkAndDelete(node) {
+    	if(node.html().replace(/\s|&nbsp;/g, '').replace('<br>').length == 0){
+		node.remove();
+		loop=true;
+	}  
+}
--- python_articleextraction/newstest.py
+++ python_articleextraction/newstest.py
@@ -7,11 +7,14 @@ Created on Sun Oct 29 17:19:51 2017
 import csv
 import feedparser
 from newspaper import Article
-import re
+import string
+import random
 
 with open('FeedsTest.CSV', 'r') as csvfile:
     feeds = csv.reader(csvfile, delimiter='|', quotechar='"')
     next(feeds)
+    a = 0
+    print("ANZAHL: " + str(a))
     for feed in feeds:
         d = feedparser.parse(feed[0])
         for entrie in d.entries:
@@ -23,26 +26,35 @@ with open('FeedsTest.CSV', 'r') as csvfile:
             
             #parse
             
-            article = Article(url, language='de', keep_article_html=True)
-            article.download()
-            article.parse()
+            #title and 
+            try:
+                article = Article(url, language='de', keep_article_html=True)
+                article.download()
+                article.parse()
             
-            filename = feed[3] + re.sub('[^a-zA-Z0-9 \n\.]', '', article.title[:8]) + ".html"
+                filename = feed[3] + ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
             
-            file = open("html/"+ filename + ".html","w")
-            article.article_html = "<meta property='baseurl' content='"+ feed[4] +"'>" + article.article_html
-            article.article_html = "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>" + article.article_html
-            article.article_html = "<link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet'>" + article.article_html
-            article.article_html = "<img src='" + article.top_image +"' width='100%' >" + article.article_html
-            article.article_html = "<h1 class='realTitle'>" + entrie.title + "</h1>" + article.article_html
-            article.article_html = article.article_html + "<link type='text/css' rel='stylesheet' href='../css/renderStyles.css'/>"
-            article.article_html = article.article_html + "<script src='../js/renderScript.js'></script>"
-            article.article_html = "<div class='pr0news pr0-text text-orange " + feed[3] + "'>" + article.article_html + "</div>"
+                file = open("html/"+ filename + ".html","w")
+                
+                article.article_html = "<meta property='baseurl' content='"+ feed[4] +"'>" + article.article_html
+                article.article_html = "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>" + article.article_html
+                article.article_html = "<link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet'>" + article.article_html
+                article.article_html = "<img src='" + article.top_image +"' width='100%' >" + article.article_html
+                article.article_html = "<h1 class='realTitle'>" + entrie.title + "</h1>" + article.article_html
+                article.article_html = article.article_html + "<link type='text/css' rel='stylesheet' href='../css/renderStyles.css'/>"
+                article.article_html = article.article_html + "<script src='../js/renderScript.js'></script>"
+                article.article_html = "<div class='pr0news pr0-text text-orange " + feed[3] + "'>" + article.article_html + "</div>"
             
-            file.write(article.article_html)
-            file.close()
+                file.write(article.article_html)
+                file.close()
             
-            if article.title != entrie.title:
-                print(str(len(article.title)) + " || " + str(len(entrie.title)))
-            else:
-                print("true")
+                if article.title != entrie.title:
+                    print(str(len(article.title)) + " || " + str(len(entrie.title)))
+                else:
+                    print("true")
+                a = a + 1
+            except:
+                print("Oops!")
+    print("ANZAHL: " + str(a))
+                
+            
